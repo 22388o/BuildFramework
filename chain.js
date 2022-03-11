4f3d961e44959dea5f99ba901df0b75ae49893f5
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2022-03-11 12:16:55 -0300

    Create chain.js

---
+++ chain.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const Chain = require('../types/chain');
+const chain = new Chain();
+
+async function main () {
+  await chain.storage.open();
+
+  chain.on('block', function (block) {
+    console.log('[CHAIN]', 'new block:', block);
+    console.log('[CHAIN]', 'chain:', chain);
+  });
+
+  chain.append({ test: 'foo' });
+  chain.append({ test: 'bar' });
+
+  await chain.storage.close();
+
+  return this;
+}
+
+main().catch((exception) => {
+  console.error('[EXAMPLES:CHAIN]', 'Main Process Exception:', exception);
+});
